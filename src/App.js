dc27fcafefdf06848d0920699fe7f97e32d87db6
TommmyKelly <tommy_kelly@icloud.com>
2021-09-26 12:05:29 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useRightClickMenu from "./hooks/useRightClickMenu";
+import Menu from "./components/Menu";
 
 function App() {
+  const { x, y, showMenu } = useRightClickMenu();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Menu x={x} y={y} showMenu={showMenu} />
+    </>
   );
 }
 
